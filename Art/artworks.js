e3c1c1eceacbea7fda0bbb01992729f947c6d214
shawCampbell <campbellshaw8@gmail.com>
2026-03-04 08:40:38 +0200

    Added content to books and art

--- Art/artworks.js
+++ Art/artworks.js
@@ -17,4 +17,58 @@ const artworks = [
     cover: "Art/Self.png"
   },
 
+  {
+    title: "Koisan Man",
+    author: "2020",
+    cover: "Art/KhoisanMan.png"
+  },
+
+  {
+    title: "Eye",
+    author: "2020",
+    cover: "Art/eye.png"
+  },
+
+  {
+    title: "Jasveer",
+    author: "2020",
+    cover: "Art/Jasveer.png"
+  },
+
+  {
+    title: "Zaian",
+    author: "2024",
+    cover: "Art/Zaian.png"
+  },
+
+  {
+    title: "Jeff Bezos",
+    author: "2019",
+    cover: "Art/Jeff.png"
+  },
+
+  {
+    title: "Getting Married",
+    author: "2020",
+    cover: "Art/GettingMarried.png"
+  },
+
+  {
+    title: "Brain",
+    author: "2020",
+    cover: "Art/Brain.png"
+  },
+
+  {
+    title: "Mouth",
+    author: "2020",
+    cover: "Art/mouth.png"
+  },
+
+  {
+    title: "Small Zaian",
+    author: "2020",
+    cover: "Art/SmallZaian.png"
+  },
+
 ];
--- Books/books.js
+++ Books/books.js
@@ -17,5 +17,52 @@ const books = [
     cover: "Books/Einsteing.png"
   },
 
+  {
+    title: "50 Physics Ideas You Really Need to Know",
+    author: "Joanne Baker",
+    cover: "Books/50PhysicsIdeas.png"
+  },
+
+  {
+    title: "Biography of Zero: The Biography of a Dangerous Idea",
+    author: "Charles Seife",
+    cover: "Books/BiographyOfZero.png"
+  },
+
+  {
+    title: "Code 2 Complete: A Practical Handbook of Software Construction",
+    author: "Steve McConnell",
+    cover: "Books/Code2Complete.png"
+  },
+
+  {
+    title: "Metamorphosis",
+    author: "Franz Kafka",
+    cover: "Books/Metamorphisi.png"
+  },
+
+  {
+    title: "Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future",
+    author: "Walter Isaacson",
+    cover: "Books/ElonMusk.png"
+  },
+
+  {
+    title: "Sapein: A Brief History of Humankind",
+    author: "Yuval Noah Harari",
+    cover: "Books/Sapiens.png"
+  },
+
+  {
+    title: "Mans Search for Meaning",
+    author: "Viktor Frankl",
+    cover: "Books/MansSearchForMeaning.png"
+  },
+
+  {
+    title: "The Innovators: How a Group of Hackers, Geniuses, and Geeks Created the Digital Revolution",
+    author: "Walter Isaacson",
+    cover: "Books/TheInnovators.png"
+  },
 
 ];
